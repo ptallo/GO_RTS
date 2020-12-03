83d0ee442915b66ca7f5c7d453d2cde87f6739ed
Philip Tallo <tallopt@mail.uc.edu>
2020-12-03 18:37:25 -0500

    clean: fixed golint and genericized Event struct

--- core/event.go
+++ core/event.go
@@ -1,18 +1,26 @@
 package core
 
-type Event struct {
-	handlers []chan bool
+// EventData is a wrapper around a generic data type
+type EventData struct {
+	data interface{}
 }
 
-func (e *Event) Subscribe(handler chan bool) {
+// Event is a class for handling firing events to handlers
+type Event struct {
+	handlers []chan EventData
+}
+
+// Subscribe allow a handler to listen for events from this source
+func (e *Event) Subscribe(handler chan EventData) {
 	if e.handlers == nil {
-		e.handlers = make([]chan bool, 0)
+		e.handlers = make([]chan EventData, 0)
 	}
 
 	e.handlers = append(e.handlers, handler)
 }
 
-func (e *Event) Unsubscribe(handler chan bool) {
+// Unsubscribe allow a handler to no longer listen for events from this source
+func (e *Event) Unsubscribe(handler chan EventData) {
 	idxToRemove := -1
 	for i, h := range e.handlers {
 		if h == handler {
@@ -28,9 +36,10 @@ func (e *Event) Unsubscribe(handler chan bool) {
 	e.handlers = e.handlers[:len(e.handlers)-1]
 }
 
-func (e *Event) Fire(data bool) {
+// Fire sends the data to all subscribed handlers
+func (e *Event) Fire(data EventData) {
 	for _, handler := range e.handlers {
-		go func(h chan bool) {
+		go func(h chan EventData) {
 			h <- data
 		}(handler)
 	}
