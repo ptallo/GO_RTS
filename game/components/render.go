31c8b145bfb7bb59bb6d907e603d57299bcda499
Philip Tallo <tallopt@mail.uc.edu>
2020-12-08 19:02:09 -0500

    clean: fixed golint issues

--- game/components/render.go
+++ game/components/render.go
@@ -7,17 +7,20 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+// IRenderComponent is an interface which describes drawing something
 type IRenderComponent interface {
 	Draw(*ebiten.Image, geometry.Point)
 	GetDrawRectangle(geometry.Point) geometry.Rectangle
 }
 
+// RenderComponent implements the IRenderComponent interface
 type RenderComponent struct {
 	spriteSheetLibrary render.ISpriteSheetLibrary
 	camera             render.ICamera
 	name               string
 }
 
+// NewRenderComponent creates a IRenderComponent object
 func NewRenderComponent(ssl render.ISpriteSheetLibrary, cam render.ICamera, name string) IRenderComponent {
 	return &RenderComponent{
 		spriteSheetLibrary: ssl,
@@ -26,6 +29,7 @@ func NewRenderComponent(ssl render.ISpriteSheetLibrary, cam render.ICamera, name
 	}
 }
 
+// Draw renders the component on the screen at the pointToDraw
 func (r *RenderComponent) Draw(screen *ebiten.Image, pointToDraw geometry.Point) {
 	opts := &ebiten.DrawImageOptions{}
 	opts.GeoM.Translate(pointToDraw.X, pointToDraw.Y)
@@ -33,6 +37,7 @@ func (r *RenderComponent) Draw(screen *ebiten.Image, pointToDraw geometry.Point)
 	ss.Draw(screen, r.camera, opts)
 }
 
+// GetDrawRectangle gives a rectangle which describes the RenderComponent at the position isoPoint
 func (r *RenderComponent) GetDrawRectangle(isoPoint geometry.Point) geometry.Rectangle {
 	ss := r.spriteSheetLibrary.GetSpriteSheet(r.name)
 	return geometry.NewRectangle(
